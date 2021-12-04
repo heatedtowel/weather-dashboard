046dc09c77e6ba4ae2d3e0e60897db034b0c3cff
Jesse Monks <monksjesse@gmail.com>
2021-12-04 14:58:49 -0500

    minor tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,13 +10,24 @@ var btnEL = document.querySelector('#cityBtn')
 var apiKey = '62b0a23cd35c692eb316b1b855d80a34';
 var days = '5'
 
+
+
+
+
+function createDate(time) {
+  var milliseconds = time * 1000
+  var dataObject = new Date(milliseconds)
+  actualDate = dataObject.toLocaleString()
+  return actualDate
+}
+
 function makeContainer(day, temp, humidity, wind, uv) {
   return `<div class="col bg-light m-2">
             <h4>Day ${day}</h4>
             <h5>${temp}&deg;</h5>
             <h5>${humidity}%</h5>
             <h5>${wind} MPH</h5>
-            <h5>${uv}</h5>
+            <h5>UV:${uv}</h5>
           </div>`
 }
 
@@ -27,36 +38,33 @@ function getCoords(city) {
     .then(data => {
       var lat = data.coord.lat;
       var lon = data.coord.lon;
-      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=imperial`)
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=imperial`)
         .then(response => {
           return response.json();
         })
         .then(data => {
-          console.log(data)
-          cityEL.textContent = `City: ${cityNameEL.value}`
-          tempEL.textContent = `Temp: ${data.daily[0].temp.day}`;
+          var unixTime = data.current.dt;
+          createDate(unixTime)
+          cityEL.textContent = `${cityNameEL.value} ${actualDate}`
+          tempEL.textContent = `${Math.floor(data.daily[0].temp.day)} degrees`;
           humidityEL.textContent = `Humidity: ${data.daily[0].humidity}`
           windEL.textContent = `Wind: ${data.daily[0].wind_speed}`
           uvEL.textContent = `UV: ${data.daily[0].uvi}`
-          console.log(data.daily[0].uvi)
 
           if (data.daily[0].uvi <= 2) {
             uvEL.style.background = 'green'
-            console.log('green')
           }
-          if (data.daily[0].uvi > 2  && data.daily[0].uvi <= 7) {
+          if (data.daily[0].uvi > 2 && data.daily[0].uvi <= 7) {
             uvEL.style.background = 'yellow'
-            console.log('yellow')
           }
           if (data.daily[0].uvi > 7) {
             uvEL.style.background = 'red'
-            console.log('red')
           }
 
           var html = ''
 
           for (var i = 1; i <= days; i++) {
-            html += makeContainer(i + 1, data.daily[i].temp.day, data.daily[i].humidity, data.daily[i].wind_speed, data.daily[i].uvi);
+            html += makeContainer(i + 1, Math.floor(data.daily[i].temp.day), data.daily[i].humidity, data.daily[i].wind_speed, data.daily[i].uvi);
           }
           parentContainer.innerHTML = html;
         })
