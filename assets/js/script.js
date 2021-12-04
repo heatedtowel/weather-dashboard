4d20bbc44129a7ef038fb81f1a29a2dd0d366094
Jesse Monks <monksjesse@gmail.com>
2021-12-03 23:53:59 -0500

    logic implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
 var cityNameEL = document.querySelector('#cityName');
 var previousResults = document.querySelector('#prevResults');
 var tempEL = document.querySelector('#temp');
+var cityEL = document.querySelector('#city');
 var humidityEL = document.querySelector('#humidity');
 var uvEL = document.querySelector('#UV');
 var parentContainer = document.getElementById('parentContainer');
+var btnEL = document.querySelector('#cityBtn')
 var apiKey = '62b0a23cd35c692eb316b1b855d80a34';
-var city = 'charlotte'
 var days = '5'
-var currentURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-var html = ''
 
 function makeContainer(day, temp, humidity, uv) {
   return `<div class="col bg-light m-2">
@@ -19,17 +18,12 @@ function makeContainer(day, temp, humidity, uv) {
           </div>`
 }
 
-
-
-
-
-getCoords();
-
-function getCoords() {
-  fetch(currentURL).then(response => {
+function getCoords(city) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`).then(response => {
     return response.json();
   })
   .then(data => {
+    console.log(data)
     var lat = data.coord.lat;
     var lon = data.coord.lon;
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=imperial`)
@@ -37,10 +31,11 @@ function getCoords() {
       return response.json();
     })
     .then(data => {
-      var currentTemp = data.daily[0].temp.day;
-      var humidity = data.daily[0].humidity
-      var uv = data.daily[0].uvi
-      console.log(currentTemp, humidity, uv)
+      cityEL.textContent = cityNameEL.value
+      tempEL.textContent = data.daily[0].temp.day;
+      humidityEL.textContent = data.daily[0].humidity
+      uvEL.textContent = data.daily[0].uvi
+      var html = ''
 
       for (var i = 1; i <= days; i++) {
         html += makeContainer(i+1, data.daily[i].temp.day, data.daily[i].humidity, data.daily[i].uvi);
@@ -50,7 +45,11 @@ function getCoords() {
   })
 }
 
-
+btnEL.addEventListener('click', function() {
+  var city = cityNameEL.value
+  console.log(city)
+  getCoords(city);
+})
 
 
 
