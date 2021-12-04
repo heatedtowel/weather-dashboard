8b5309c32c13c63162900c0902c8a2898dce4fbb
Jesse Monks <monksjesse@gmail.com>
2021-12-04 00:34:04 -0500

    additional features

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var cityNameEL = document.querySelector('#cityName');
 var previousResults = document.querySelector('#prevResults');
 var tempEL = document.querySelector('#temp');
 var cityEL = document.querySelector('#city');
+var windEL = document.querySelector('#wind');
 var humidityEL = document.querySelector('#humidity');
 var uvEL = document.querySelector('#UV');
 var parentContainer = document.getElementById('parentContainer');
@@ -9,11 +10,12 @@ var btnEL = document.querySelector('#cityBtn')
 var apiKey = '62b0a23cd35c692eb316b1b855d80a34';
 var days = '5'
 
-function makeContainer(day, temp, humidity, uv) {
+function makeContainer(day, temp, humidity, wind, uv) {
   return `<div class="col bg-light m-2">
             <h4>Day ${day}</h4>
             <h5>${temp}&deg;</h5>
             <h5>${humidity}%</h5>
+            <h5>${wind} MPH</h5>
             <h5>${uv}</h5>
           </div>`
 }
@@ -22,39 +24,49 @@ function getCoords(city) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`).then(response => {
     return response.json();
   })
-  .then(data => {
-    console.log(data)
-    var lat = data.coord.lat;
-    var lon = data.coord.lon;
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=imperial`)
-    .then(response => {
-      return response.json();
-    })
     .then(data => {
-      cityEL.textContent = cityNameEL.value
-      tempEL.textContent = data.daily[0].temp.day;
-      humidityEL.textContent = data.daily[0].humidity
-      uvEL.textContent = data.daily[0].uvi
-      var html = ''
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=imperial`)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          console.log(data)
+          cityEL.textContent = `City: ${cityNameEL.value}`
+          tempEL.textContent = `Temp: ${data.daily[0].temp.day}`;
+          humidityEL.textContent = `Humidity: ${data.daily[0].humidity}`
+          windEL.textContent = `Wind: ${data.daily[0].wind_speed}`
+          uvEL.textContent = `UV: ${data.daily[0].uvi}`
+          console.log(data.daily[0].uvi)
 
-      for (var i = 1; i <= days; i++) {
-        html += makeContainer(i+1, data.daily[i].temp.day, data.daily[i].humidity, data.daily[i].uvi);
-      }
-      parentContainer.innerHTML = html;
+          if (data.daily[0].uvi <= 2) {
+            uvEL.style.background = 'green'
+            console.log('green')
+          }
+          if (data.daily[0].uvi > 2  && data.daily[0].uvi <= 7) {
+            uvEL.style.background = 'yellow'
+            console.log('yellow')
+          }
+          if (data.daily[0].uvi > 7) {
+            uvEL.style.background = 'red'
+            console.log('red')
+          }
+
+          var html = ''
+
+          for (var i = 1; i <= days; i++) {
+            html += makeContainer(i + 1, data.daily[i].temp.day, data.daily[i].humidity, data.daily[i].wind_speed, data.daily[i].uvi);
+          }
+          parentContainer.innerHTML = html;
+        })
     })
-  })
 }
 
-btnEL.addEventListener('click', function() {
+btnEL.addEventListener('click', function () {
   var city = cityNameEL.value
-  console.log(city)
+  h3EL = document.createElement('h3')
+  h3EL.textContent = city
+  previousResults.appendChild(h3EL)
   getCoords(city);
 })
-
-
-
-
-
-
-/* "lon": -80.8431,
-"lat": 35.2271 */
